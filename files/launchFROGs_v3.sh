6a491b81d50e70ba89471483412dc176076b8fcc
Julie ORJUELA <julie.orjuela@ird.fr>
2019-05-20 15:15:24 +0200

    adding $1 as parameter

--- files/launchFROGs_v3.sh
+++ files/launchFROGs_v3.sh
@@ -11,7 +11,10 @@
 #$ -l h_vmem=10G
 
 ############################################################
-MOI="formationX" ####### A MODIFIER
+
+[[ -z ${1} ]] || exit 1
+
+MOI="formation${1}"
 REMOTE_FOLDER="nas:/home/$MOI/TP-FROGS"
 READS_SAMPLE='nas:/data2/formation/TPMetabarcoding/FROGS/DATA_s.tar.gz' #### A MODIFIER
 TMP_FOLDER="/scratch/$MOI-$JOB_ID"; 
